69d5175f522fd528393122931976e4a0a71bd643
PrgmrBill <bill+github@prgmrbill.com>
2015-07-04 14:06:14 -0400

    Adds service methods as line items, implements optional delivery fee

--- app/orderForm/orderForm.js
+++ app/orderForm/orderForm.js
@@ -10,13 +10,27 @@ angular.module('pizzaTime.orderForm', ['ngRoute', 'checklist-model'])
 }])
 
 .controller('OrderFormController', ["$scope", "$http", function ($scope, $http) {
-	$scope.order = {		
+	$scope.order = {
 		total: 0,
 		pieSize: "Large",
-		items: []
+		items: [],
+		deliveryMethod: "carryout"
 	};
 
+	$scope.onDeliveryMethodChanged = function (method) {
+		if (method.price === 0) {
+			removeDeliveryFee();
+		} else {
+			method.name = "Delivery Fee";
+			
+			$scope.addLineItem(method);
+		}
+	};
+	
 	$http.get("orderForm/order-form.json").success(function (data) {
+		$scope.pieSizes = data.pieSizes;
+		$scope.settings = data.settings;
+
 		$scope.toppings = {
 			meat: (function () {
 				var items = [];
@@ -43,9 +57,6 @@ angular.module('pizzaTime.orderForm', ['ngRoute', 'checklist-model'])
 			})()
 		};
 
-		$scope.pieSizes = data.pieSizes;
-		$scope.minimumOrderAmount = data.settings.minimumOrderAmount;
-
 	}).error(function (xhr, status, error) {
 		console.error(error);
 	});
@@ -71,6 +82,7 @@ angular.module('pizzaTime.orderForm', ['ngRoute', 'checklist-model'])
 		var exists = false;
 
 		for (var j = 0; j < items.length; j++) {
+			// Item already in order
 			if (items[j] === item) {
 				if (item.quantity < items[j].maxQuantity) {
 					items[j].quantity += 1;
@@ -80,6 +92,7 @@ angular.module('pizzaTime.orderForm', ['ngRoute', 'checklist-model'])
 			}
 		}
 
+		// New line item
 		if (!exists) {
 			items.push(item);
 		}
@@ -87,12 +100,16 @@ angular.module('pizzaTime.orderForm', ['ngRoute', 'checklist-model'])
 
 	$scope.modifyLineItemQuantity = function (item) {
 		for (var j = 0; j < $scope.order.items.length; j++) {
+			// Item found
 			if ($scope.order.items[j] === item) {
+				// If there is one left, it's effectively removed
 				if (item.quantity === 1) {
-					item.quantity = 0;
 					$scope.order.items.splice(j, 1);			
 				} else {
-					item.quantity -= 1;
+					// If there is more than one of this item, decrement quantity
+					if (item.quantity > 1) {
+						item.quantity -= 1;
+					}
 				}
 
 				break;
@@ -101,6 +118,16 @@ angular.module('pizzaTime.orderForm', ['ngRoute', 'checklist-model'])
 	};
 
 	$scope.isOrderValid = function () {
-		$scope.order.total >= minOrderAmount;
+		$scope.order.total >= $scope.settings.minimumOrderAmount;
 	};
+
+	function removeDeliveryFee() {
+		var items = $scope.order.items;
+
+		for (var j = 0; j < items.length; j++) {
+			if (items[j].type === "serviceMethod" && items[j].price > 0) {
+				items = items.splice(j, 1);
+			}
+		}
+	}
 }]);
