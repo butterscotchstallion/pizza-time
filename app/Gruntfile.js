765bba559ef9c9a0d97d8e76c93c42f51857364f
PrgmrBill <bill+github@prgmrbill.com>
2015-07-12 15:20:06 -0400

    Fixes watch/restart express

---
+++ app/Gruntfile.js
@@ -0,0 +1,59 @@
+module.exports = function(grunt) {
+    var env = require("./config/env.json");
+
+    grunt.initConfig({
+        express: {
+            dev: {
+                options: {
+                    script: env.expressPath
+                }
+            }
+        },
+        watch: {
+            express: {
+                files:  ['Gruntfile.js',   
+                         'app.js', 
+                         'routes/**/*.js',
+                         'test/*.js'],
+                tasks:  ['express:dev'],
+                options: {
+                    spawn: false
+                }
+            },
+            css: {
+                files: ['scss/**/*.scss'],
+                tasks: ['sass'],
+                options: {
+                    spawn: false
+                }
+            }
+        },
+        sass: {
+            dist: {
+                options: {
+                    style: "compressed"
+                },
+                files: {
+                    'public/build/screen.min.css': 'scss/screen.scss'
+                }
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-express-server');
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    
+    grunt.registerTask('server', ['express:dev', 'watch:express']);
+    grunt.registerTask('watchassets', [
+        'watch:css'
+    ]);
+    
+    grunt.registerTask('default', ['server']);
+};
+
+
+
+
+
+
