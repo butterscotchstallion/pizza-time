24a0124faed06005089fa4aaa041bf146e84a420
PrgmrBill <bill+github@prgmrbill.com>
2015-07-19 08:44:25 -0400

    Refactors app to move routes in and adds accountAccessServiceTokenService

--- app/public/app.js
+++ app/public/app.js
@@ -1,17 +1,44 @@
-'use strict';
+"use strict";
 
 // Declare app level module which depends on views, and components
-angular.module('pizzaTime', [
-  'ngRoute',
-  'ngAnimate',
-  //'templateCacheModule',
-  'angular-loading-bar',
-  'pizzaTime.findALocation',
-  'pizzaTime.orderForm',
-  'ui.bootstrap'
-]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({
-  	redirectTo: "/order/find-a-location"
-  });
+var app = angular.module("pizzaTime", [
+  "ngRoute",
+  "ngAnimate",
+  "LocalStorageModule",
+  "angular-loading-bar",
+  "ui.bootstrap",
+  "angularMoment"
+]);
+
+app.config(["$routeProvider", "$locationProvider", function ($routeProvider, $locationProvider) {
+	$routeProvider.when('/order/:guid', {
+    	templateUrl: 'components/order-form/order-form.html',
+    	controller: 'OrderFormController'
+  	});
+
+	$routeProvider.when('/account/dashboard', {
+    	templateUrl: 'components/account/dashboard/index.html',
+    	controller: 'DashboardController'
+  	});
+
+	$routeProvider.when('/account/sign-in', {
+    	templateUrl: 'components/account/sign-in/index.html',
+    	controller: 'SignInController'
+  	});
+
+  	$routeProvider.when("/order/find-a-location", {
+    	templateUrl: "components/find-a-location/index.html",
+    	controller: "FindALocationController"
+  	});
+
+  	$routeProvider.otherwise({
+  		redirectTo: "/account/sign-in"
+  	});
+
+  	$locationProvider.html5Mode(true);
+  	$locationProvider.hashPrefix('!');
+}]);
+
+app.run(["accountAccessTokenService", function (accountAccessTokenService) {
+	accountAccessTokenService.populateSessionData();
 }]);
